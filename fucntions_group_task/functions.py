dac06dd208d929e6c9d54d29a6e99a6b2580917e
AmbiaaH <ambiaahussain@gmail.com>
2025-11-13 21:51:08 +0000

    functions group task and day four tasks

---
+++ fucntions_group_task/functions.py
@@ -0,0 +1,9 @@
+def print_something():
+    print("testing")
+print_something()
+
+
+def print_something(name):
+    print("Hello", name)
+print_something("Ambia")
+
---
+++ functions/calculator.py
@@ -0,0 +1,16 @@
+def addition(num1, num2): #define function and add arguments
+    return num1 + num2 #returns argument behaviour (adds both numbers)
+print(addition(5, 3))#provides parameters for num1 and num2
+
+def subtraction(num1, num2):
+    return num1 - num2
+print(subtraction(11, 4))
+
+
+def multiplication(num1, num2):
+    return num1 * num2
+print(multiplication(12, 6))
+
+def division(num1, num2):
+    return num1 / num2
+print(division(50, 4))
